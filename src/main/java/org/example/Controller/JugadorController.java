f3f7d4f7c541c25ca686d6bfe984346555872e2b
mespinosa7 <manelespinosa653@gmail.com>
2024-03-08 23:05:18 +0100

    asegurar rutas controller jugador

--- src/main/java/org/example/Controller/JugadorController.java
+++ src/main/java/org/example/Controller/JugadorController.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.example.Model.User;
 import org.example.Service.JugadorService;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,14 +17,14 @@ public class JugadorController {
     private final JugadorService jugadorService;
 
     @GetMapping("/findAll")
-    //@PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ROLE_Admin')")
     @ResponseStatus(HttpStatus.OK)
     public List<User> getPlayers() {
         return jugadorService.findAll();
     }
 
     @GetMapping("/{username}")
-    //@PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ROLE_User')")
     @ResponseStatus(HttpStatus.OK)
     public User getUserByUsername(@PathVariable String username) {
         return jugadorService.findByUsername(username);
