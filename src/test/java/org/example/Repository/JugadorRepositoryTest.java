da42c21fc986fa85c0342064ca41258754085729
davidval2022 <davidvalentin74@gmail.com>
2024-03-15 20:43:30 +0100

    javadoc en el archivo de test

--- src/test/java/org/example/Repository/JugadorRepositoryTest.java
+++ src/test/java/org/example/Repository/JugadorRepositoryTest.java
@@ -12,7 +12,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * Esta clase contiene pruebas unitarias para el repositorio de jugadores (JugadorRepository).
+ * Las pruebas se centran en probar el funcionamiento de los métodos del repositorio, como guardar
+ * un jugador en la base de datos y buscar un jugador por su nombre de usuario.
+ */
 @SpringBootTest
 class JugadorRepositoryTest {
 
@@ -25,25 +29,31 @@ class JugadorRepositoryTest {
     private User user1;
     private Role role1;
     private Role role2;
-
+    /**
+     * Configura el entorno para las pruebas creando roles y usuarios de ejemplo en la base de datos.
+     */
     @BeforeEach
     void setUp() {
-
-
 
         role1=roleRepository.save(new Role( ERole.ROLE_User));
         role2=roleRepository.save(new Role( ERole.ROLE_Admin));
         user1 = jugadorRepository.save(new User( "manel","manelesp","123456",role1));
 
-
     }
-
+    /**
+     * Limpia el entorno después de las pruebas eliminando todos los usuarios y roles creados.
+     */
     @AfterEach
     void tearDown() {
         jugadorRepository.deleteAll();
         roleRepository.deleteAll();
     }
-
+    /**
+     * Prueba si se puede guardar un nuevo jugador correctamente en el repositorio.
+     * Se crea un nuevo usuario y se guarda en el repositorio de jugadores.
+     * Luego se comprueba si el usuario se puede recuperar correctamente del repositorio
+     * utilizando el método findByUsername.
+     */
     @Test
     void save_jugador_successful() {
 
@@ -51,7 +61,12 @@ class JugadorRepositoryTest {
         assertEquals(user2,jugadorRepository.findByUsername("manelesp1").get());
 
     }
-
+    /**
+     * Prueba si se puede encontrar un jugador por su nombre de usuario en el repositorio.
+     * Se intenta recuperar un jugador por su nombre de usuario ("manelesp") y se verifica
+     * si el jugador recuperado es igual al jugador creado en el método setUp.
+     * Además, se verifica si no se puede encontrar ningún jugador con un nombre de usuario inexistente ("Boing").
+     */
     @Test
     void findByUsername() {
         User jugador = jugadorRepository.findByUsername("manelesp").get();
