dd86556f6f45ef3c6905ba7a0de242691709f982
mespinosa7 <manelespinosa653@gmail.com>
2024-04-06 13:15:31 +0200

    evitar null pointers

--- src/main/java/org/example/mapper/JugadorMapper.java
+++ src/main/java/org/example/mapper/JugadorMapper.java
@@ -5,6 +5,10 @@ import org.example.DTOs.ParejaDTO;
 import org.example.Model.Pareja;
 import org.example.Model.User;
 
+import java.util.Collections;
+import java.util.Objects;
+import java.util.OptionalInt;
+
 public class JugadorMapper {
 
     public JugadorDTO mapJugadorToJugadorDTO(User jugador){
@@ -26,19 +30,25 @@ public class JugadorMapper {
 
     private Integer getPartidasGanadas(User jugador) {
         int partidasGanadasJugador1 = jugador.getParejasComoJugador1().stream()
-                .mapToInt(e->e.getPartidasGanadas().size()).sum();
+                .mapToInt(e-> Objects.requireNonNullElse(e.getPartidasGanadas(), Collections.emptyList()).size()).sum();
         int partidasGanadasJugador2=jugador.getParejasComoJugador2().stream()
-                .mapToInt(e->e.getPartidasGanadas().size()).sum();
+                .mapToInt(e->Objects.requireNonNullElse(e.getPartidasGanadas(), Collections.emptyList()).size()).sum();
 
-        return partidasGanadasJugador1+partidasGanadasJugador2;
+        int totalPartidasGanadas = partidasGanadasJugador1 + partidasGanadasJugador2;
+
+        // Devolver el total de partidas ganadas o 0 si es que no hay ninguna
+        return OptionalInt.of(totalPartidasGanadas).orElse(0);
     }
 
     private Integer getPartidasPerdidas(User jugador) {
         int partidasPerdidasJugador1 = jugador.getParejasComoJugador1().stream()
-                .mapToInt(e->e.getPartidasPerdidas().size()).sum();
+                .mapToInt(e->Objects.requireNonNullElse(e.getPartidasPerdidas(), Collections.emptyList()).size()).sum();
         int partidasPerdidasJugador2=jugador.getParejasComoJugador2().stream()
-                .mapToInt(e->e.getPartidasPerdidas().size()).sum();
+                .mapToInt(e->Objects.requireNonNullElse(e.getPartidasPerdidas(), Collections.emptyList()).size()).sum();
 
-        return partidasPerdidasJugador1+partidasPerdidasJugador2;
+        int totalPartidasPerdidas = partidasPerdidasJugador1 + partidasPerdidasJugador2;
+
+        // Devolver el total de partidas ganadas o 0 si es que no hay ninguna
+        return OptionalInt.of(totalPartidasPerdidas).orElse(0);
     }
 }
